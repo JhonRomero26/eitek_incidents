3b18e3903962e17d9e54d115e0886b6aab9a8fb8
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2026-01-15 20:20:09 -0500

    chore: add initial backend structure with spring boot for java

---
+++ backend/src/test/java/com/eitek/incidents/IncidentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eitek.incidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
