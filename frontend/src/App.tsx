68589520b59fc478a62b99e4438277cf64b9241e
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2026-01-15 20:31:29 -0500

    chore: implement auth pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Switch } from "wouter";
+
+// Paginas
+import LoginPage from "@/app/pages/login";
+import RegisterPage from "@/app/pages/register";
+
 export function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+    </Switch>
   );
 }
 
